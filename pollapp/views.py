197585fdfc0832d9205fe2a07fbe9b7590db7b36
seamussmith <student.141504@worcesterschools.net>
2021-01-13 17:37:56 -0500

    added post handler

--- pollapp/views.py
+++ pollapp/views.py
@@ -1,8 +1,10 @@
-from django.http.response import Http404
+from django.http.response import Http404, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.POST:
+        return HttpResponse(f"{request.POST}")
     return render(request, "pages/index.html", context={})
 
 def contact(request):
