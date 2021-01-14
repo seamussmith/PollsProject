6670f702a58677c16cb82761ad2a6d8405b3ffdb
seamussmith <student.141504@worcesterschools.net>
2021-01-14 14:44:44 -0500

    scripts are now written in typescript

--- static/scripts/poll-script.js
+++ static/scripts/poll-script.js
@@ -1,20 +1,18 @@
-
-function SendVote(self)
-{
-    let $self = $(self)
-    let $form = $(self.parentElement)
-    console.log($form)
+function SendVote(self) {
+    let $self = $(self);
+    let $form = $(self.parentElement);
+    console.log($form);
     $.ajax({
         method: $form.attr("method"),
         data: `${$form.serialize()}&${$self.attr("name")}=${$self.attr("value")}`,
         url: $form.attr("action")
     })
-    .done((result) => {
+        .done((result) => {
         console.log(result);
-        $form.children().prop("disabled", false)
+        $form.children().prop("disabled", false);
     })
-    .fail((e) => {
-        alert(e)
-    })
-    $form.children().prop("disabled", true)
+        .fail((e) => {
+        alert(e);
+    });
+    $form.children().prop("disabled", true);
 }
---
+++ static/scripts/poll-script.ts
@@ -0,0 +1,20 @@
+
+function SendVote(self)
+{
+    let $self = $(self)
+    let $form = $(self.parentElement)
+    console.log($form)
+    $.ajax({
+        method: $form.attr("method"),
+        data: `${$form.serialize()}&${$self.attr("name")}=${$self.attr("value")}`,
+        url: $form.attr("action")
+    })
+    .done((result) => {
+        console.log(result);
+        $form.children().prop("disabled", false)
+    })
+    .fail((e) => {
+        alert(e)
+    })
+    $form.children().prop("disabled", true)
+}
