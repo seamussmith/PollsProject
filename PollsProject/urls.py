baf03d1823a297e4b2e17e43c009a79494680b10
seamussmith <student.141504@worcesterschools.net>
2021-01-14 13:27:43 -0500

    Added rooturl path

--- PollsProject/urls.py
+++ PollsProject/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
+from django.shortcuts import redirect
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("pollapp/", include("pollapp.urls"))
+    path("pollapp/", include("pollapp.urls")),
+    path("", include("rooturl.urls"))
 ]
