821972a495ba7d1256fb4e78c082f974179fe37f
seamussmith <student.141504@worcesterschools.net>
2021-01-14 14:16:52 -0500

    Added CreatePoll inclusion tag

--- pollapp/templatetags/my_tags.py
+++ pollapp/templatetags/my_tags.py
@@ -1,4 +1,5 @@
 from django.template.defaulttags import register
+from django.template.loader import render_to_string
 
 @register.filter()
 def seq(mini, maxi):
@@ -8,3 +9,9 @@ def seq(mini, maxi):
 def mul(a, b):
     return str(float(a) * float(b))
  
+@register.inclusion_tag()
+def CreatePoll(poll, i=0):
+    return render_to_string("components/poll.html", context={
+        i: i,
+        poll: poll
+    })
