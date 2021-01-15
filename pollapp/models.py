6ac3f384d931405b4a0958010c40f0a95a97f0e5
seamussmith <student.141504@worcesterschools.net>
2021-01-14 19:36:00 -0500

    helper function to create a new poll

--- pollapp/models.py
+++ pollapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from uuid import uuid4
+from datetime import date
+import json
 
 # Create your models here.
 class Poll(models.Model):
@@ -6,3 +9,16 @@ class Poll(models.Model):
     choices = models.TextField()
     uuid = models.TextField()
     creation_date = models.DateTimeField()
+
+def CreatePoll(name, choices, uuid=None, creation_date=None):
+    if uuid is None:
+        uuid = uuid4()
+    if creation_date is None:
+        creation_date = date.today().isoformat()
+    str_choices = json.dumps(choices)
+    return Poll(
+        name = name,
+        choices = str_choices,
+        uuid = uuid,
+        creation_date = creation_date
+    )
