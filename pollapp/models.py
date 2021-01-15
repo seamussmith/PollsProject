251042c5028493cf894bbf76fd37bbc417580d76
seamussmith <student.141504@worcesterschools.net>
2021-01-14 20:05:04 -0500

    added utility methods to Poll class

--- pollapp/models.py
+++ pollapp/models.py
@@ -3,15 +3,24 @@ from uuid import uuid4
 from datetime import date
 import json
 
+
 # Create your models here.
 class Poll(models.Model):
     name = models.TextField()
     choices = models.TextField()
     uuid = models.TextField(primary_key=True)
 
-def NewPoll(data):
-    return Poll(
-        name = data["name"],
-        choices = json.dumps(data["choices"]),
-        uuid = data.get("uuid") or str(uuid4()),
-    )
+    @staticmethod
+    def new(data):
+        return Poll(
+            name = data["name"],
+            choices = json.dumps(data["choices"]),
+            uuid = data.get("uuid") or str(uuid4()),
+        )
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "choices": json.loads(self.choices),
+            "uuid": self.uuid
+        }
