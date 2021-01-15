3bde665d1c188b91187cb8000f5047a6fd4dbf08
seamussmith <student.141504@worcesterschools.net>
2021-01-14 19:29:39 -0500

    Started working on database

--- pollapp/models.py
+++ pollapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    name = models.TextField()
+    choices = models.TextField()
+    uuid = models.TextField()
+    creation_date = models.DateTimeField()
