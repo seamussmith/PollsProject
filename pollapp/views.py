ac1f54b2003fd1852ca2921c6dae6fb6199d1e38
seamussmith <student.141504@worcesterschools.net>
2021-01-15 20:16:20 -0500

    Added comments to index function

--- pollapp/views.py
+++ pollapp/views.py
@@ -7,15 +7,20 @@ import json
 
 # Create your views here.
 def index(request):
-    if request.POST:
+    if request.POST: # If the request is a POST requset, assuming this is a vote...
         body = request.POST
+        # Get the uuid of the poll and the user's choice
         uuid = body.get("uuid")
         choice = int(body.get("choice"))
+        # Query the database for the poll
         poll = next(i for i in Poll.objects.all() if i.uuid == uuid)
+        # Increment the choice that the user chose by 1
         poll.inc_vote(choice, 1)
+        # Save and return the new data for the poll as a JSON string
         poll.save()
         return HttpResponse(json.dumps(poll.to_dict()))
-    polls = [i.to_dict() for i in Poll.objects.all()][:20] # Grab the first 20 polls
+    # Else, assume it is a user trying to access the website
+    polls = [i.to_dict() for i in Poll.objects.all()][:20] # Grab the first 20 polls you can get from the database
     return render(request, "pages/index.html", context={
         "polls": polls
     })
