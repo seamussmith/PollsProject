29ba9bda2eea74ed9b0434760b1e636bf708a3a2
seamussmith <student.141504@worcesterschools.net>
2021-01-19 10:02:24 -0500

    use sessions to prevent duplicate votes

--- pollapp/views.py
+++ pollapp/views.py
@@ -9,6 +9,8 @@ from math import ceil, floor
 
 # Create your views here.
 def index(request):
+    if not request.session.get("votes"):
+        request.session["votes"] = {}
     if request.POST: # If the request is a POST requset, assuming this is a vote...
         body = request.POST
         # Get the uuid of the poll and the user's choice
@@ -17,13 +19,18 @@ def index(request):
         # Query the database for the poll
         poll = Poll.objects.get(uuid=uuid)
         # Increment the choice that the user chose by 1
-        poll.inc_vote(choice, 1)
-        # Save and return the new data for the poll as a JSON string
-        poll.save()
+        if uuid in request.session["votes"]:
+            poll.inc_vote(request.session["votes"][uuid], -1)
+            request.session["votes"].pop(uuid)
+        if not choice == request.session["votes"].get(uuid):
+            poll.inc_vote(choice, 1)
+            # Save and return the new data for the poll as a JSON string
+            poll.save()
+            #total = reduce(lambda x, y: x + y["votes"], data["choices"], 0)
+            #for i in data["choices"]:
+            #    i["percent"] = i["votes"]/total * 100
         data = poll.to_dict()
-        #total = reduce(lambda x, y: x + y["votes"], data["choices"], 0)
-        #for i in data["choices"]:
-        #    i["percent"] = i["votes"]/total * 100
+        request.session["votes"][uuid] = choice
         return HttpResponse(json.dumps(data))
     # Else, assume it is a user trying to access the website
     polls = [i.to_dict() for i in Poll.objects.all()][:20] # Grab the first 20 polls you can get from the database
