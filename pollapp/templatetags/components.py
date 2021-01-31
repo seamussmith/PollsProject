b1c8c2a79620441ee09e7229b7a9266d754d35e4
seamussmith <student.141504@worcesterschools.net>
2021-01-31 14:44:09 -0500

    CreatePoll now utilizes vote to render a voted poll

--- pollapp/templatetags/components.py
+++ pollapp/templatetags/components.py
@@ -1,8 +1,9 @@
 from django.template.defaulttags import register
 
 @register.inclusion_tag("components/poll.html")
-def CreatePoll(poll, delay=0):
+def CreatePoll(poll, vote, delay=0):
     return {
         "delay": delay,
-        "poll": poll
+        "poll": poll,
+        "vote": vote
     }
