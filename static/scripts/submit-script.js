9616cbaf3762d5769ca3e3b7749352856744adf9
seamussmith <student.141504@worcesterschools.net>
2021-03-09 11:21:07 -0500

    Account for changes in directories

--- static/scripts/submit-script.js
+++
@@ -1,6 +0,0 @@
-function AddChoice(self) {
-    if ($(`input[type="text"]`).length < 6)
-        $(self).before(`<input type="text" name="choice[]" maxlength="20">`);
-    if ($(`input[type="text"]`).length === 6)
-        $(self).prop("disabled", true);
-}
