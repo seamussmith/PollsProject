71e7a8c392c5292f90f4ac698c0873ee54ada5c4
seamussmith <student.141504@worcesterschools.net>
2021-03-24 11:49:54 -0400

    Admin fields

--- pollapp/admin.py
+++ pollapp/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Poll)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
 
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question']
+    list_display = (
+        "question",
+        "pub_date",
+    )
+    fieldsets = [
+        ('Question', {
+            'fields': ['question']
+        }),
+        ('Date', {
+            'fields': ['pub_date']
+        }),
+        ('UUID', {
+            'fields': ['uuid']
+        })
+    ]
+    inlines = [
+        ChoiceInline
+    ]
+
+admin.site.register(Poll, QuestionAdmin)
+admin.site.register(Choice)
